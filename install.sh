777eb1da35db0ff4cf8000a0d738c0197b92b9df
derekharget <git@derekharget.com>
2017-07-29 10:38:01 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#
-#
+# Buggy but should do about 99% of the work for you.
+# Notice anything off, open a issue
 #
 
 
@@ -10,6 +10,22 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
+
+
+function escape_slashes {
+    sed 's/\//\\\//g' 
+}
+
+function change_line {
+    local OLD_LINE_PATTERN=$1; shift
+    local NEW_LINE=$1; shift
+    local FILE=$1
+
+    local NEW=$(echo "${NEW_LINE}" | escape_slashes)
+    sed -i .bak '/'"${OLD_LINE_PATTERN}"'/s/.*/'"${NEW}"'/' "${FILE}"
+    mv "${FILE}.bak" /tmp/
+}
+
 
 #Move to the /root directory
 cd /root
@@ -45,6 +61,17 @@ cd php-7.1.7
 ./configure --enable-fpm --with-pdo-mysql --with-zlib --with-bz2 --enable-zip --with-openssl --with-mhash --with-curl --with-gd --with-jpeg-dir --with-png-dir --enable-ftp --enable-exif --with-freetype-dir --enable-calendar --enable-soap --enable-mbstring --with-libxml-dir=/usr/lib --with-mysqli > /dev/null
 make -j4 > /dev/null
 make install > /dev/null
+php.ini-production /usr/local/lib/php.ini
+
+cd /usr/local/etc/ && cp -rf php-fpm.conf.default php-fpm.conf
+
+#Adjust the NONE to the local directory
+change_line "include=NONE/etc/php-fpm.d/*.conf" "include=/usr/local/etc/php-fpm.d/*.conf" php-fpm.conf
+
+cd php-fpm.d
+
+cp -fr www.conf.default www.conf
+
 echo "PHP 7.1.7 Successfully Installed"
 
 #Back to temp directory
